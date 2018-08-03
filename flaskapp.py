7c64c693d5986ead7877ead743b983d4dc65b83d
Oumaïma MITA <mita.oumaima@gmail.com>
2018-08-03 22:40:59 +0100

    test1

---
+++ flaskapp.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+
+#WA[
+from whatsapp import Client
+expected_token = 'mySecretToken'
+#]WA
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello World!'
+
+@app.route('/msg', methods = ['POST'])
+def msg():
+    to = request.form['to']
+    return str(to)
+
+@app.route('/sendmsg')
+def sendmsg():
+    to = request.args.get('to')
+    msg = request.args.get('msg')
+    token = request.args.get('token')
+    if(str(token) == expected_token):
+        client = Client(login='212669976775', password='1NGskIeMhhci4kxpEgP1j/7NWOs=')
+        res = client.send_message(to, msg)  
+    
+    else:
+        res = 'Unauthorized'
+    
+    return str(res)
+
+if __name__ == '__main__':
+    app.run()
